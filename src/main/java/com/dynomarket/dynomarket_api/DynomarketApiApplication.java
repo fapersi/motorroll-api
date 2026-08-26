ea409c2ed15cdc38c22437856345d5c30beffc01
¨fatima <fpersidanniaux@uade.edu.ar>
2026-08-26 15:47:59 -0300

    base base del proyecto

---
+++ src/main/java/com/dynomarket/dynomarket_api/DynomarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynomarket.dynomarket_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynomarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynomarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynomarket/dynomarket_api/DynomarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynomarket.dynomarket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynomarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
